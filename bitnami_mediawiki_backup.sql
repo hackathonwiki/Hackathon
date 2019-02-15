c8849e2e75a445b30a9d113a91a550044a1843db
Shresth Gupta <shresthgupta054@gmail.com>
2019-02-15 10:01:01 -0500

    Bitnami_mediawiki database backup dated 15th Feb

---
+++ bitnami_mediawiki_backup.sql
@@ -0,0 +1,1 @@
+OCI runtime exec failed: exec failed: container_linux.go:344: starting container process caused "exec: \"/usr/bin/mysqldump\": stat /usr/bin/mysqldump: no such file or directory": unknown
